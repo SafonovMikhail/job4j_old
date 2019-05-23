f1daa0e7f6eaadc29a80012774414f0980818203
Mikhail Safonov <ms33@inbox.ru>
2019-05-23 06:10:31 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j.calculate;
+/*
+*Calculate
+*
+*@author Mikhail Safonov (ms33@inbox.ru)
+*
+*/
 public class Calculate {
+	/*
+	*Конструктор вывода строки в консоль
+	*
+	*@param arg - arg.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Mikhail Safonov (ms33@inbox.ru.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
