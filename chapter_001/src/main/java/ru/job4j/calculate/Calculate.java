a180f46e44cdabba992a7162425a6ed04ddd980f
Mikhail Safonov <ms33@inbox.ru>
2019-06-06 22:16:43 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,7 +3,8 @@ package ru.job4j.calculate;
 *Calculate
 *
 *@author Mikhail Safonov (ms33@inbox.ru)
-*
+*@version $Id$
+*@since 0.1
 */
 public class Calculate {
 	/*
@@ -14,4 +15,16 @@ public class Calculate {
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}
+	/*
+	* Method echo.
+	* @param name Your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
+
+
+
+
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+* Package for calculate task.
+*
+* @author Safonov Mikhail (ms33@inbox.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
