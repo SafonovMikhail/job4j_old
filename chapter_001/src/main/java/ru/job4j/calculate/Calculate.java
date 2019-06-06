a48d726c64fec3837af9b99cdae872454cf72434
Mikhail Safonov <ms33@inbox.ru>
2019-06-06 22:46:33 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -12,7 +12,7 @@ public class Calculate {
 	*
 	*@param arg - arg.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 	}
 	/*
