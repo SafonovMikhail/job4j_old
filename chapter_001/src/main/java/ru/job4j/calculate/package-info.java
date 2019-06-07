4793b82efedba6d162df73a8089f707b28bf73e5
Mikhail Safonov <ms33@inbox.ru>
2019-06-07 05:48:55 +0300

    test

--- chapter_001/src/main/java/ru/job4j/calculate/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -1,8 +1,9 @@
 /**
 * Package for calculate task.
 *
-* @author Mikhail Safonov (ms33@inbox.ru.ru)
+* @author Mikhail Safonov (ms33@inbox.ru)
 * @version $Id$
 * @since 0.1
+*
 */
 package ru.job4j.calculate;
